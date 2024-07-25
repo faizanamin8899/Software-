0a5e6b85a03829822ddcc4d07f82b68950152383
faizanamin8899 <muzammilamin912@gmail.com>
2024-07-25 08:42:58 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+// function zee(){
+//     var w ;
+//     var r = document.getElementById('in').value
+//     switch(r){
+//        case "300":
+//           w="ali is all paper pass"
+//           break
+//           case "200":
+//           w="han sub ok hai"
+//  break
+ 
+//     }
+//     document.getElementById('of').value=w
+//  }
+
+function jee(){
+   var j;
+   var r ;
+   var e;
+   var s ;
+   var w =document.getElementById('in').value
+   switch(w){
+      case "gobispaint":
+      s="9000"
+      e='30'
+      r=e*s
+      // j= new Date() 
+      // j.getDay()
+      break
+      case "kenwood":
+         s="2400"
+         e ="1000"
+         r=e*s
+break
+   }
+document.getElementById('of').value=s 
+document.getElementById('ji').value=e
+document.getElementById('ki').value=r
+// document.getElementById('oi').value=j  //
+}
